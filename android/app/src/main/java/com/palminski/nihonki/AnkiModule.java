b074a3648d1be86994e6011bab5dbb61157d831e
Will <williambolls98@gmail.com>
2025-09-22 16:42:08 -0700

    can make requests to gpr-4o-mini and create cards

--- android/app/src/main/java/com/palminski/nihonki/AnkiModule.java
+++ android/app/src/main/java/com/palminski/nihonki/AnkiModule.java
@@ -3,14 +3,19 @@ package com.palminski.nihonki;
 import android.database.Cursor;
 import android.net.Uri;
 import android.util.Log;
+import android.util.SparseArray;
 
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.json.JSONArray;
 
+import com.ichi2.anki.api.NoteInfo;
 import com.ichi2.anki.api.AddContentApi;
 
 public class AnkiModule extends ReactContextBaseJavaModule {
@@ -33,6 +38,83 @@ public class AnkiModule extends ReactContextBaseJavaModule {
     // Methods To Be Called From Native App
 
     // ==============================================================================================================
+
+    @ReactMethod
+    public void addNote(
+            String kanji,
+            String kana,
+            String furigana,
+            String meaning,
+            String partOfSpeech,
+            String exampleSentenceKanji,
+            String exampleSentenceFurigana,
+            String exampleSentenceKana,
+            String exampleSentenceEnglish,
+            Promise promise
+            ) {
+        try {
+            String pkg = AddContentApi.getAnkiDroidPackageName(reactContext);
+            if (pkg == null) {
+                promise.reject("ANKI_UNAVAILABLE", "ANKIDROID API is not available");
+                return;
+            }
+
+            // Ensure Deck Exists
+            Long deckId = helper.findDeckIdByName("DEV DECK");
+            if (deckId == null) {
+                deckId = helper.getApi().addNewDeck("DEV DECK");
+            }
+
+            Long modelId = helper.findModelIdByName("Core 2000", 18);
+            if (modelId == null) {
+                promise.resolve("Core 2000 Note Type Not Found");
+                return;
+            }
+
+            List<String> keys = Arrays.asList(kanji);
+            SparseArray<List<NoteInfo>> duplicateNotes = helper.getApi().findDuplicateNotes(modelId, keys);
+            if(duplicateNotes != null && duplicateNotes.size() > 0) {
+                promise.resolve("Duplicate Found. Note Not Added");
+                return;
+            }
+
+            long noteId = helper.getApi().addNote(
+                    modelId,
+                    deckId,
+                    new String[] {
+                            kanji, // 1. Index
+                            kanji, // 2. Vocab Kanji
+                            furigana, // 3. Vocab Kanji With Furigana
+                            kana, // 4. Vocab Kana
+                            meaning, // 5. Vocabulary English
+                            "", // 6. Vocabulary Audio
+                            partOfSpeech, // 7. Vocabulary Pos (Type of Speech)
+                            "", // 8. Caution
+                            exampleSentenceKanji, // 9. Example Sentance Kanji
+                            exampleSentenceFurigana, // 10. Example Sentance Kanji with Furigana
+                            exampleSentenceKana, // 11. Example Sentence Only Kana
+                            exampleSentenceEnglish, // 12. English Sentences
+                            "", // 13. Sentence Clozed (?)
+                            "", // 14. Sentence Audio
+                            "", // 15. Notes
+                            kanji, // 16. Core Index
+                            kanji, // 17. Optimised Sentence Index
+                            "", // 18. Fequency
+                    },
+                    null);
+            // "日の出", "sunrise"
+            if (noteId == -1) {
+                promise.resolve("Duplicate note skipped");
+            } else {
+                promise.resolve("Note added with Id: " + noteId);
+            }
+
+            promise.resolve("Note Added");
+        } catch (Exception e) {
+            Log.e("AnkiModule", "ERROR ADDING TEST NOTE", e);
+            promise.reject("ANKI_ERROR", e);
+        }
+    }
 
     @ReactMethod
     public void addTestNote(Promise promise) {
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,16 @@
 module.exports = function (api) {
   api.cache(true);
-  let plugins = ["react-native-reanimated/plugin", "react-native-worklets/plugin"];
+  let plugins = [
+    "react-native-reanimated/plugin",
+    "react-native-worklets/plugin",
+    [
+      "module:react-native-dotenv",
+      {
+        "moduleName": "@env",
+        "path": ".env"
+      }
+    ]
+  ];
 
   return {
     presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
