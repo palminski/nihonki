d6c8cb994a2e40cdbbc78d06baf555d4abf1b48c
Will <williambolls98@gmail.com>
2025-09-22 00:03:55 -0700

    was able to add a deck AND a card in said deck

--- src/screens/TestScreen.tsx
+++ src/screens/TestScreen.tsx
@@ -1,21 +1,45 @@
-import { View, Text, Pressable, Alert } from "react-native";
+
+import { View, Text, Pressable, Alert, NativeModules } from "react-native";
 import ScreenWrapper from "~/components/ScreenWrapper";
+
+const { AnkiModule } = NativeModules;
 
 export default function TestScreen() {
 
-    const DebugFunction = () =>
-    {
-        Alert.alert("button pressed!");
+    const DebugFunction = async () => {
+
+        try {
+            const result = await AnkiModule.addTestNote();
+            alert(result);
+
+        } catch (error) {
+            console.error("ERROR FETCHING DECKS!", error);
+            Alert.alert("ERROR: ");
+        }
     }
+
+    // const RequestPermission = async () => {
+
+    //     try {
+    //         const result = await AnkiModule.requestPermission();
+    //         alert(result);
+
+    //     } catch (error) {
+    //         console.error("ERROR FETCHING DECKS!", error);
+    //         Alert.alert("ERROR: ");
+    //     }
+    // }
 
     return (
         <ScreenWrapper>
             <Text className="text-white">
                 Anki Testing
             </Text>
-            <Pressable className="bg-purple-900 rounded p-2" onPress={DebugFunction}>
-                <Text>Click Here</Text>
+            <Pressable className="bg-purple-500/50 rounded p-2" onPress={DebugFunction}>
+                <Text>Add Card</Text>
             </Pressable>
+
+            
         </ScreenWrapper>
     )
 }
