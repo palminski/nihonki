b37eb7034d8ec26cc6c15d7ade9cda05f0230e99
Will <williambolls98@gmail.com>
2025-09-21 21:18:24 -0700

    preparing for android version

--- src/screens/TestScreen.tsx
+++ src/screens/TestScreen.tsx
@@ -1,12 +1,21 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable, Alert } from "react-native";
 import ScreenWrapper from "~/components/ScreenWrapper";
 
 export default function TestScreen() {
-    return(
+
+    const DebugFunction = () =>
+    {
+        Alert.alert("button pressed!");
+    }
+
+    return (
         <ScreenWrapper>
             <Text className="text-white">
-                Test
+                Anki Testing
             </Text>
+            <Pressable className="bg-purple-900 rounded p-2" onPress={DebugFunction}>
+                <Text>Click Here</Text>
+            </Pressable>
         </ScreenWrapper>
     )
 }
