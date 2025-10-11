4bcf65e5721314bfeb129c02b94e5c4c33ad685b
Will <williambolls98@gmail.com>
2025-10-10 18:38:32 -0700

    updated package lock again

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = function (api) {
   api.cache(true);
   let plugins = [
     "react-native-reanimated/plugin",
-    "react-native-worklets/plugin",
+    ["react-native-worklets/plugin"],
   ];
 
   return {
