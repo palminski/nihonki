a1f740b67b4a9ed5b21b3a7435ed84fbdd56d1f3
Will <williambolls98@gmail.com>
2025-10-15 12:32:13 -0700

    programatically added a card type

--- android-custom/AnkiModule.java
+++ android-custom/AnkiModule.java
@@ -63,7 +63,8 @@ public class AnkiModule extends ReactContextBaseJavaModule {
                 return;
             }
 
-            if (!handlePermissions(promise)) return;
+            if (!handlePermissions(promise))
+                return;
 
             // Ensure Deck Exists
             Long deckId = helper.findDeckIdByName(deckToInsertInto);
@@ -71,9 +72,12 @@ public class AnkiModule extends ReactContextBaseJavaModule {
                 deckId = helper.getApi().addNewDeck(deckToInsertInto);
             }
 
-            Long modelId = helper.findModelIdByName("Core 2000", 18);
+            Long modelId = helper.findModelIdByName("Nihonki Card", 18);
             if (modelId == null) {
-                promise.resolve("Core 2000 Note Type Not Found");
+                modelId = addNihonkiNoteType(deckId);
+            }
+            if (modelId == null) {
+                promise.resolve("Model Could Not Be Added");
                 return;
             }
 
@@ -134,7 +138,8 @@ public class AnkiModule extends ReactContextBaseJavaModule {
                 return;
             }
 
-            if (!handlePermissions(promise)) return;
+            if (!handlePermissions(promise))
+                return;
 
             Long modelId = helper.findModelIdByName("Core 2000", 18);
             if (modelId == null) {
@@ -174,6 +179,132 @@ public class AnkiModule extends ReactContextBaseJavaModule {
     @ReactMethod
     public void checkAndRequestPermissions(Promise promise) {
         handlePermissions(promise);
+    }
+
+    // ==============================================================================================================
+
+    // Private Methods
+
+    // ==============================================================================================================
+
+    private Long addNihonkiNoteType(Long deckIdString) {
+        AddContentApi api = helper.getApi();
+        String[] fields = new String[] {
+                "Optimized-Voc-Index",
+                "Vocabulary-Kanji",
+                "Vocabulary-Furigana",
+                "Vocabulary-Kana",
+                "Vocabulary-English",
+                "Vocabulary-Audio",
+                "Vocabulary-Pos",
+                "Caution",
+                "Expression",
+                "Reading",
+                "Sentence-Kana",
+                "Sentence-English",
+                "Sentence-Clozed",
+                "Sentence-Audio",
+                "Notes",
+                "Core-Index",
+                "Optimized-Sent-Index",
+                "Frequency",
+
+        };
+        String frontTemplate = """
+                <div class="japanese" style="font-size:60px;">{{Vocabulary-Kanji}}</div>
+
+                <div style="font-size: 16px; ">{{Vocabulary-Pos}}</div>
+                <br/><br/><br/><br/><br/>
+                <div id="example-sentence" class="japanese" style="font-size:40px;">{{Expression}}</div>
+                                                """;
+        String backTemplate = """
+                <div>{{Vocabulary-Audio}}{{Sentence-Audio}}</div>
+                <div class="japanese" style="font-size:60px;">{{furigana:Vocabulary-Furigana}}</div>
+
+                <div style="font-size: 14px; ">{{Vocabulary-Pos}}</div>
+
+                <hr id=answer>
+
+                <div style="font-size: 30px; ">{{Vocabulary-English}}</div>
+
+                <div class="japanese" style="font-size: 50px; ">{{furigana:Reading}}</div>
+                <div style="font-size: 20px; ">{{Sentence-English}}</div>
+                <!---->
+
+                <div style="font-size: 14pt;">
+                <a class="japanese ios-only" href="mkdaijirin://jp.monokakido.DAIJIRIN/search?text={{Vocabulary-Kanji}}&srcname=Anki&src=anki://">大辞林</a><a class="japanese ios-only" href="mkwisdom2://jp.monokakido.WISDOM2/search?text={{Vocabulary-Kanji}}&srcname=Anki&src=anki://">ウィズダム</a><a class="japanese" href="http://dictionary.goo.ne.jp/srch/thsrs/{{Vocabulary-Kanji}}/m0u/">類語辞典</a><a class="ios-only" href="ebpocket://search?text={{Vocabulary-Kanji}}#anki://">EBPocket</a><a href="http://jisho.org/search/{{Vocabulary-Kanji}}">jisho</a><a href="http://ejje.weblio.jp/content/{{Vocabulary-Kanji}}">weblio</a>
+                <br/>
+                <div style="font-size: 10pt;">{{Frequency}}</div>
+                                                """;
+        String css = """
+                                @font-face { font-family: stroke; src: url('_stroke.ttf'); }
+                @font-face { font-family: textbook; src: url('_HGSKyokashotai.ttf'); }
+                @font-face {
+                   font-family: 'notosans';
+                   font-style: normal;
+                   font-weight: 400;
+                   src: url(http://fonts.gstatic.com/ea/notosansjapanese/v6/NotoSansJP-Regular.otf) format('opentype');
+                 }
+                @font-face {
+                   font-family: 'localnoto';
+                   font-style: normal;
+                   font-weight: 400;
+                   src: url('_NotoSansJP-Medium.otf') format('opentype');
+                 }
+
+                .card {
+                 font-family:  'localnoto', 'notosans', 'ヒラギノ明朝 ProN', 'Hiragino Mincho Pro', 'serif';
+                 font-size: 25px;
+                 text-align: center;
+                 color: White;
+                 background-color: Black;
+                }
+
+                img {
+                 max-height: 150px;
+                 margin: 10px;
+                }
+
+                div {
+                 padding-top:5px;
+                 padding-bottom:5px;
+                }
+
+                a {
+                 color: turquoise;
+                 margin: 5px;
+                }
+
+                .ios-only { display: none; }
+                .mac-only { display: none; }
+
+                .mobile .ios-only { display: inline; }
+                .mac .mac-only { display: inline; }
+
+                .stroke { font-family: "stroke"; }
+                .japanese { font-family: "textbook"; }
+
+                .mac { font-family: 'ヒラギノ明朝 ProN', 'Hiragino Mincho Pro', 'serif'; }
+                .mac .stroke { font-family: stroke; }
+                .mac .japanese { font-family: textbook; }
+
+                .mobile { font-family: textbook, 'Hiragino Mincho Pro', 'serif'; }
+                                """;
+        String[] cards = new String[] { "Recognition" };
+        String[] qfmt = new String[] { frontTemplate };
+        String[] afmt = new String[] { backTemplate };
+
+        Long newModelId = api.addNewCustomModel(
+            "Nihonki Card",
+            fields,
+            cards,
+            qfmt,
+            afmt,
+            css,
+            deckIdString,
+            0
+        );
+        return newModelId;
     }
 
     private boolean handlePermissions(Promise promise) {
